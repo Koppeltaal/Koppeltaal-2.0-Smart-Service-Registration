dcc7c814660ec5acf2063a7fe253d99803b8f0cf
Joris Scharp <jorisscharp@gmail.com>
2021-05-14 16:32:54 +0200

    Initial Spring Boot initializr project

---
+++ src/main/java/nl/koppeltaal/smartserviceregistration/SmartServiceRegistrationApplication.java
@@ -0,0 +1,13 @@
+package nl.koppeltaal.smartserviceregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartServiceRegistrationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SmartServiceRegistrationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/nl/koppeltaal/smartserviceregistration/SmartServiceRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.koppeltaal.smartserviceregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartServiceRegistrationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
