6631882db080439e0a1881bf622ab67ea39be99f
Joris Scharp <jorisscharp@gmail.com>
2021-05-25 09:51:35 +0200

    Running integration test against in in-memory DB to simplify the build / Dockerfile creation. Testcontainers can be added when it's required to specifically run integration tests against MySQL.

--- src/test/java/nl/koppeltaal/smartserviceregistration/SmartServiceRegistrationApplicationTests.java
+++ src/test/java/nl/koppeltaal/smartserviceregistration/SmartServiceRegistrationApplicationTests.java
@@ -1,41 +1,12 @@
 package nl.koppeltaal.smartserviceregistration;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import nl.koppeltaal.smartserviceregistration.SmartServiceRegistrationApplicationTests.Initializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
-@Testcontainers
-@ContextConfiguration(initializers = {Initializer.class})
 class SmartServiceRegistrationApplicationTests {
 
-  @Container
-  private static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.25"));
-
   @Test
-  void contextLoads() {
-    assertTrue(MY_SQL_CONTAINER.isRunning());
-  }
-
-  static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-      TestPropertyValues.of(
-          "spring.datasource.url=" + MY_SQL_CONTAINER.getJdbcUrl(),
-          "spring.datasource.username=" + MY_SQL_CONTAINER.getUsername(),
-          "spring.datasource.password=" + MY_SQL_CONTAINER.getPassword()
-      ).applyTo(configurableApplicationContext.getEnvironment());
-    }
-  }
+  void contextLoads() { }
 
 }
